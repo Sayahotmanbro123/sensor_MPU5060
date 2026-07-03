b35120ac354e8f59c0c69c0844872fbfd03878e0
Hotman Pangaribuan <hotmanpangiribuan17@gmail.com>
2026-07-03 17:25:41 +0700

    tambah variabel error

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -3,6 +3,13 @@
 #include "freertos/task.h"
 
 void app_main() {
+  bool error = true;
+  if (error) {
+    printf("Error occurred!\n");
+  }
+  else {
+    printf("Hello, Wokwi!\n");
+  }
   printf("Hello, Wokwi!\n");
   while (true) {
     printf("Hello, Wokwi!\n");
